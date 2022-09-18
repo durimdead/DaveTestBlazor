c8eac712e1237e0d5b82d7f4ab6c974c5bc17d1a
david.lancellotti <david.lancellotti@gmail.com>
2022-09-18 10:44:17 -0400

    added user controller

---
+++ DaveTestBlazor/Server/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using DaveTestBlazor.Server.Repositories;
+using DaveTestBlazor.Server.Services.Interfaces;
+using DaveTestBlazor.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DaveTestBlazor.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(IUserService userService, ILogger<UserController> logger)
+        {
+            _userService = userService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _userService.GetAllUsers();
+        }
+    }
+}
